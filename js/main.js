ba791e009ebd1c9b4a30880e07f84c7cd555ec45
hamad edine izidine <izdine54@icloud.com>
2026-08-02 15:46:26 +0000

    js:filtre des intervenants, validation du formulaire

--- js/main.js
+++ js/main.js
@@ -41,4 +41,106 @@ if (btnTop) {
             behavior: "smooth"
         });
     });
+
+    // filtre des intervenants
+}
+let filterButtons = document.querySelectorAll(".filter-btn");
+let speakerCards = document.querySelectorAll(".speaker-card");
+
+for (let i = 0; i < filterButtons.length; i++) {
+    filterButtons[i].addEventListener("click", function () {
+        let categorieChoisie = this.getAttribute("data-filter");
+
+        for (let j = 0; j < filterButtons.length; j++) {
+            filterButtons[j].classList.remove("active");
+        }
+        this.classList.add("active");
+
+        for (let k = 0; k < speakerCards.length; k++) {
+            let carte = speakerCards[k];
+            let categorieCarte = carte.getAttribute("data-category");
+
+            if (categorieChoisie === "tous" || categorieCarte === categorieChoisie) {
+                carte.style.display = "block";
+            } else {
+                carte.style.display = "none";
+            }
+        }
+    });
+}
+
+// validation du formulaire
+let formulaire = document.getElementById("registrationForm");
+if (formulaire) {
+    
+formulaire.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    let valide = true;
+
+    let nom = document.getElementById("fullName");
+    let erreurNom = document.getElementById("fullNameError");
+
+    if(nom.value.trim() == ""){
+        erreurNom.textContent = "Veuillez entrer votre nom complet.";
+        valide = false;
+    }
+    else{
+        erreurNom.textContent = "";
+    }
+    let email = document.getElementById("email");
+    let erreurEmail = document.getElementById("emailError");
+
+    if(email.value.trim() == ""){
+        erreurEmail.textContent = "Veuillez entrer votre email.";
+        valide = false;
+    }
+    else{
+        erreurEmail.textContent = "";
+    }
+
+    let telephone = document.getElementById("phone");
+    let erreurTelephone = document.getElementById("phoneError");
+
+    if(telephone.value.trim() == ""){
+        erreurTelephone.textContent = "Veuillez entrer votre téléphone.";
+        valide = false;
+    }
+    else{
+        erreurTelephone.textContent = "";
+
+}
+        let participationType = document.getElementById("participationType");
+        let erreurParticipation = document.getElementById("participationTypeError");
+        if (participationType.value === "") {
+            erreurParticipation.textContent = "Choisis un type de participation.";
+            valide = false;
+        } else {
+            erreurParticipation.textContent = "";
+        }
+
+        let country = document.getElementById("country");
+        let erreurCountry = document.getElementById("countryError");
+        if (country.value === "") {
+            erreurCountry.textContent = "Choisis ton pays.";
+            valide = false;
+        } else {
+            erreurCountry.textContent = "";
+        }
+        let message = document.getElementById("message");
+        let erreurMessage = document.getElementById("messageError");
+        if (message.value.trim() === "") {
+            erreurMessage.textContent = "Veuillez écrire un message.";
+            valide = false;
+        } else {
+            erreurMessage.textContent = "";
+        }
+        if (valide === true) {
+            let successMsg = document.getElementById("successMessage");
+            successMsg.style.display = "block";
+            successMsg.textContent = "Formulaire soumis avec succès !";
+            formulaire.reset();
+        }
+    });
 }
